3e10288fe794deed98f23bbcfeb66bfe16c08b80
Riya Jain <riya2007@student.ubc.ca>
2026-05-20 00:37:34 +0400

    Set up basic Flask structure with templates and routing

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Your Flask app is working 🎉"
+    return render_template("index.html")
+
+@app.route("/listings")
+def listings():
+    return render_template("listings.html")
+
+@app.route("/create")
+def create():
+    return render_template("create-listing.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
